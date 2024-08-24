55fed423fc0914e314960662bde2a65c0fcbc1a6
mike-neck <jkrt3333@gmail.com>
2024-08-25 01:39:31 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.github.mwiede:jsch:0.2.19")
+    implementation("org.bouncycastle:bcprov-jdk18on:1.78")
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
